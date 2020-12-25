a3b44b5b08aeaea7bdd65520d85916d049a7cfbe
AhmetSaitCalisir <56252214+AhmetSaitCalisir@users.noreply.github.com>
2020-12-25 16:10:20 +0300

    v0.7.0
    Navbar Component düzenlenmesi

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  users: User[] = [new User(0, 'KULLANICI YÜKLENİYOR', 'KULLANICI YÜKLENİYOR')];
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.userService.getUsers().subscribe((users) => {
+      this.users = users;
+    });
   }
-
 }
--- src/app/components/user/user-item/user-item.component.ts
+++ src/app/components/user/user-item/user-item.component.ts
@@ -12,6 +12,7 @@ export class UserItemComponent implements OnInit {
     'KULLANICI YÜKLENİYOR',
     'KULLANICI YÜKLENİYOR'
   );
+
   constructor() {}
 
   ngOnInit(): void {}
